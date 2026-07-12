4b5eb65133af1a9456b51021f13406048552fb2f
Andrii Zelenskyi <Zelenskiy2013@gmail.com>
2026-07-12 18:40:16 +0300

    fix according linter v1.1

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import os
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) == 3 and parts[0] == "cp":
-        file_1 = parts[1]
-        file_2 = parts[2]
-        if os.path.exists(file_1):
-            if file_1 != file_2:
-                with (open(file_1, "r") as file_in,
-                      open(file_2, "w") as file_out):
+        source_file_name = parts[1]
+        destination_file_name = parts[2]
+        if os.path.exists(source_file_name):
+            if source_file_name != destination_file_name:
+                with (open(source_file_name, "r") as file_in,
+                      open(destination_file_name, "w") as file_out):
                     file_out.write(file_in.read())
