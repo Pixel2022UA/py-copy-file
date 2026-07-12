185811f19aa85a1ff2ae2b00fde8e1b7d389672b
Andrii Zelenskyi <Zelenskiy2013@gmail.com>
2026-07-12 18:32:44 +0300

    fix according linter

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ import os
 
 
 def copy_file(command: str) -> None:
-        if len(command.split()) == 3 and command.split()[0] == "cp":
-            file_1 = command.split()[1]
-            file_2 = command.split()[2]
-            if os.path.exists(file_1):
-                if file_1 != file_2:
-                    with open(file_1, "r") as file_in, open(file_2, "w") as file_out:
-                        file_out.write(file_in.read())
+    if len(command.split()) == 3 and command.split()[0] == "cp":
+        file_1 = command.split()[1]
+        file_2 = command.split()[2]
+        if os.path.exists(file_1):
+            if file_1 != file_2:
+                with (open(file_1, "r") as file_in,
+                      open(file_2, "w") as file_out):
+                    file_out.write(file_in.read())
